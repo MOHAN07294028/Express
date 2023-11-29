36ab9539557c70040bb5ac9fba9ab9ca4c79e373
MOHAN07294028 <nmoha4028@gmail.com>
2023-11-29 16:47:09 +0530

    express-CRUD

---
+++ Routes/users.js
@@ -0,0 +1,99 @@
+const express = require('express')
+const router = express.Router()
+
+let data = [
+    {
+        name:'Mohan',
+        age:23
+    },
+    {
+        name:'kaviya',
+        age:21
+    },
+    {
+        name:'Nagutha',
+        age:25
+    }
+]
+
+// GET METHOD
+router.get('/numbers',(req,res)=>{
+    res
+    .status(200)
+    .send({
+        message:'Success',
+        Items_0:data
+    })
+})
+
+// GET METHOD WITH ID
+router.get('/:ind',(req,res)=>{
+    if(Number(req.params.ind)<data.length){
+        res
+        .status(200)
+        .send({
+            message:'Success',
+            Items_0:data[req.params.ind]
+        })
+    }
+    else{
+        res.status(400).send('Data illa da...')
+    }
+   
+})
+
+// POST METHOD 
+router.post('/',(req,res)=>{
+    data.push(req.body)
+    res
+    .status(201)
+    .send({
+        message:'Added',
+        Items_0:data
+    })
+})
+
+// PUT METHOD
+router.put('/:ind',(req,res)=>{
+    if(Number(req.params.ind) < data.length){
+        data.splice(req.params.ind,1,req.body)
+        res
+        .status(200)
+        .send({
+            message:'Update successfully',
+            Items_0:data
+        })
+    }
+    else{
+        res
+        .status(400)
+        .send({
+            message:'Vaipu illa da raja...'
+        })
+    }
+})
+
+// DELETE METHOD
+router.delete('/:ind',(req,res)=>{
+    if(Number(req.params.ind < data.length)){
+        data.splice(req.params.ind,1)
+        res
+        .status(200)
+        .send({
+            message:'Delete Successfully',
+            Items_0:data
+        })
+    }
+    else{
+        res
+        .status(400)
+        .send({
+            message:'Ethir pakra data illa da nanbaa...'
+        })
+    }
+    
+})
+
+
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,25 @@
+console.log('Mohanraj Srinivasan')
+const express = require('express');
+const loginCall = require('./Routes/login')
+const logoutCall = require('./Routes/logout')
+const usersCall = require('./Routes/users')
+const app = express();
+app.use(express.json());
+
+app.use('/login',loginCall)
+app.use('/logout',logoutCall)
+app.use('/users',usersCall)
+
+// app.post('/mohan',(req,res)=>{
+//     console.log(req.body)
+//     res.send('Data Saved Successfully')
+// })
+
+// app.get('/',(req,res)=>{
+//     res.send('<h1>Mohanraj Srinivasan</h1>')
+// })
+
+
+
+
+app.listen(8000,()=>console.log('App is running'))
