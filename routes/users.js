1b799ca8142894aea81ea0337c90663207772545
MOHAN07294028 <nmoha4028@gmail.com>
2023-12-03 15:55:02 +0530

    String Check

--- routes/users.js
+++ routes/users.js
@@ -79,7 +79,7 @@ routes.put('/:id',async (req,res)=>{
         let data =await db.collection('users').updateOne({_id: new mongoDB.ObjectId(req.params.id)},{$set:req.body})
         res.status(200).send({
             resultBoolean:true,
-            data:'Data updated successfully'
+            data:'Data updated successfully da Mohan'
         })
     } catch (error) {
         res.status(400).send({
