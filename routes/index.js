daac7a9b1dfd440d39b2b4ed0e68d3a094e28b07
MOHAN07294028 <nmoha4028@gmail.com>
2023-12-04 10:17:44 +0530

    mangose-env-latest

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const { UserModal } = require("../schema/UserSchema")
-const { dbUrl } = require("../config/dbConnection")
-const routes = express.Router()
-mongoose.connect(dbUrl)
+const express = require("express");
+const mongoose = require("mongoose");
+const { UserModal } = require("../schema/UserSchema");
+const { dbUrl } = require("../config/dbConnection");
+const routes = express.Router();
+mongoose.connect(dbUrl);
 
 routes.get("/", async (req, res) => {
   try {
@@ -44,19 +44,18 @@ routes.get("/:id", async (req, res) => {
 
 routes.post("/", async (req, res) => {
   try {
-    let users = await UserModal.findOne({email:req.body.email});
-    if(!users){
-        let newUsers = await UserModal.create(req.body)
-        res.status(200).send({
-            resultBoolean: true,
-            data: "Data Saved Successfully",
-          });
-    }
-    else{
-        res.status(400).send({
-            resultBoolean: true,
-            data: "Data already exist",
-          });
+    let users = await UserModal.findOne({ email: req.body.email });
+    if (!users) {
+      let newUsers = await UserModal.create(req.body);
+      res.status(200).send({
+        resultBoolean: true,
+        data: "Data Saved Successfully",
+      });
+    } else {
+      res.status(400).send({
+        resultBoolean: true,
+        data: "Data already exist",
+      });
     }
   } catch (error) {
     res.status(400).send({
@@ -68,34 +67,33 @@ routes.post("/", async (req, res) => {
 
 routes.put("/:id", async (req, res) => {
   try {
-      let user = await UserModal.findById(req.params.id)
-      if(user){
-        user.name = req.body.name
-        user.email = req.body.email
-        user.password = req.body.password
-        await user.save()
-        res.status(200).send({
-            resultBoolean: true,
-            data: "Data updated successfully",
-          });
-      }
-      else{
-        res.status(400).send({
-            resultBoolean: false,
-            data: "Invalid User",
-          });
-      }
+    let user = await UserModal.findById(req.params.id);
+    if (user) {
+      user.name = req.body.name;
+      user.email = req.body.email;
+      user.password = req.body.password;
+      await user.save();
+      res.status(200).send({
+        resultBoolean: true,
+        data: "Data updated successfully",
+      });
+    } else {
+      res.status(400).send({
+        resultBoolean: false,
+        data: "Invalid User",
+      });
+    }
   } catch (error) {
     res.status(400).send({
       resultBoolean: false,
-      data:error?.message
+      data: error?.message,
     });
   }
 });
 
 routes.delete("/:id", async (req, res) => {
   try {
-    let user = await UserModal.findByIdAndDelete(req.params.id)
+    let user = await UserModal.findByIdAndDelete(req.params.id);
     res.status(200).send({
       resultBoolean: true,
       data: "Deleted Successfully",
